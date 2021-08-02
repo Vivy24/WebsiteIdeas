488e23fea54b392a1f39b5b83762fdb93d3d8e6c
Vivy <vngvy48@gmail.com>
2021-08-02 21:43:03 +0000

    CS50 FINAL PROJECT

---
+++ helpers.py
@@ -0,0 +1,31 @@
+import os
+import requests
+
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+def login_required(f):
+
+    """
+
+    Decorate routes to require login.
+
+
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+
+    """
+
+    @wraps(f)
+
+    def decorated_function(*args, **kwargs):
+
+        if session.get("user_id") is None:
+
+            return render_template("login.html",message="Please log in")
+
+        return f(*args, **kwargs)
+
+    return decorated_function
