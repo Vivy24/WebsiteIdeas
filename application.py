e36ad00bca788d807e9b4a7dd15ff7336703a238
Vivy Vuong <vngvy48@gmail.com>
2022-03-18 10:23:20 -0400

    Update application.py

--- application.py
+++ application.py
@@ -58,7 +58,7 @@ def register():
 
 @app.route("/guestLogin")
 def guestLogin():
-    rows = db.execute("SELECT * FROM users WHERE username = "BAOVY1234")
+    rows = db.execute("SELECT * FROM users WHERE username = BAOVY1234)
     # Remember which user has logged in
     session["user_id"] = rows[0]["id"]
     return redirect("/lists")
